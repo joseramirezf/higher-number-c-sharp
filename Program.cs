c0a39ce6ca38853c652f6ba905ca849c40f83c80
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-07-07 14:19:21 -0600

    Higher Number in C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Higher Number");
+
+Console.WriteLine("Add Amount of numbers:");
+int[] number = new int[Int32.Parse(Console.ReadLine() ?? "0")];
+
+for (int i = 0; i < number.Length; i++)
+{
+Console.WriteLine("Add a Number:");
+string input = Console.ReadLine() ?? "0";
+number[i] = Int32.Parse(input);
+}
+
+int higher = number.Max();
+
+Console.WriteLine("The higher number is: " + higher);
